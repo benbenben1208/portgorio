42f221efb41bce7a541d2bf52ff1cd28c1abf4bd
benbenben1208 <ari.ka1208@gmail.com>
2020-04-20 22:45:56 +0900

    ナビコンポーネント作成

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand navbar-dark purple-gradient">
+  <a class="navbar-brand" href="#">
+    <i class="fas fa-camera-retro"></i> 
+  </a>
+  
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="#">ユーザー登録</a>  
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">ログイン</a>  
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">投稿する</a>  
+    </li>
+
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+       >
+        <i class="fas fa-user-circle"></i>    
+      </a>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+        <button class="dropdown-item" type="button" onclick="location.href=''">
+          ログアウト
+        </button>
+      </div>
+    </li>
+    <form method="POST" action="" id="logout-button"></form>
+  </ul>   
+
+
+
+
+</nav>
