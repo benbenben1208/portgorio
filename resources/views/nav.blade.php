e7c9ce0a592ce30e4a47e79622e2accf8e5854e6
benbenben1208 <ari.ka1208@gmail.com>
2020-04-21 18:57:59 +0900

    ナビバー作成

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,28 +4,39 @@
   </a>
   
   <ul class="navbar-nav ml-auto">
+    @guest
     <li class="nav-item">
-      <a class="nav-link" href="#">ユーザー登録</a>  
+    <a class="nav-link" href="{{ route('register')}}">ユーザー登録</a>  
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">ログイン</a>  
+    <a class="nav-link" href="{{ route('login')}}">ログイン</a>  
     </li>
+    @endguest
+    @auth
     <li class="nav-item">
-      <a class="nav-link" href="#">投稿する</a>  
+      <a class="nav-link" href="">投稿する</a>  
     </li>
-
     <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-       >
-        <i class="fas fa-user-circle"></i>    
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+         aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-user-circle"></i>
       </a>
-      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item" type="button" onclick="location.href=''">
+      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+        <button class="dropdown-item" type="button"
+      onclick="location.href='{{ route('users.show',[ Auth::id() , 'user_id'] )}}'">
+          マイページ
+        </button>
+        <div class="dropdown-divider"></div>
+        <button form="logout-button" class="dropdown-item" type="submit">
           ログアウト
         </button>
       </div>
     </li>
-    <form method="POST" action="" id="logout-button"></form>
+    
+    <form method="POST" action="{{ route('logout')}}" id="logout-button">
+    @csrf
+  </form>
+    @endauth
   </ul>   
 
 
