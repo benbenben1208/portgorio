5c83f1d7d7bfab4f13cad1a26206c50d87721993
benbenben1208 <ari.ka1208@gmail.com>
2020-04-22 21:10:50 +0900

    投稿フォーム

---
+++ resources/views/posts/form.blade.php
@@ -0,0 +1,11 @@
+<div class="form-group d-flex flex-row align-items-center">
+  <div>
+    <img class="rounded-circle" width="100" height="100" src="{{ asset('storage/user_images/' . Auth::user()->id . '.jpg')}}" >
+  </div>  
+  <div class="col">
+    <input type="text" name="caption" placeholder="キャプションを書く" class="form-control"　value="{{$post->caption?? old('caption')}}">  
+  </div>
+</div>
+<div>
+  <input type="file" name="photo" accept="image/jpeg,image/gif,image/png" >  
+</div>
