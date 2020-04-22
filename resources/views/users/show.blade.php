a8ad07034223199c2bb1b2147227f8143db28b35
benbenben1208 <ari.ka1208@gmail.com>
2020-04-22 21:08:39 +0900

    ユーザーページ

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,19 +3,22 @@
 @section('content')
   @include('nav')
   <div class="container">
-    <div class="card">
+    <div class="card mt-5">
       <div class="card-body">
-        <div class="flex flex-row">
-          <a href="{{ route('users.show' , ['user_id' => $user->id ])}}">
+        <div class="flex flex-row mx-auto">
+          <a href="{{ route('users.show' , ['user' => $user->id ])}}">
             @if($user->profile_photo)  
-              <img src="{{ asset('/users_images' . $user->profile_photo)}}" alt="#">
+              <img class="rounded-circle" height="100" width="100" src="{{ asset('storage/user_images/' . $user->profile_photo)}}" alt="#">
             @else
               <i class="fas fa-user-circle fa-5x"></i>
             @endif
-          </a>    
+          </a>
+          @if($user->id == Auth::id())
+        　　<a class="btn btn-pink btn-xs" href="{{ route('users.edit',['user' => $user->id ])}}">編集</a>
+        　@endif
         </div>
-          <h2 class="h5 card-title m-0">
-            <a class="text-dark" href="{{ route('users.show' , ['user_id' => $user->id ])}}">
+          <h2 class="h5 card-title m-0 pt-1 pl-2" >
+            <a class="text-dark" href="{{ route('users.show' , ['user' => $user->id ])}}">
               {{ $user->name}}
             </a>    
           </h2>    
