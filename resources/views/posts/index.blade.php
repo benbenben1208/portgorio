e6532f8e482e451a66ef75ec61dbfaed16783a79
benbenben1208 <ari.ka1208@gmail.com>
2020-04-22 21:10:03 +0900

    投稿一覧ページ

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,5 +2,27 @@
 @section('title', '一覧')
 @include('nav')
 @section('content')
-
+  <div class="container">
+    @foreach($posts as $post)  
+    <div class="card">
+       
+      <div class="card-header d-flex flex-row">
+        <a href="{{ route('users.show', ['user' => $post->user->id ])}}"> 
+            @if($post->user->profile_photo)  
+            <img class="rounded-circle" height="100" width="100" src="{{ asset('storage/user_images/' . $post->user->profile_photo)}}" alt="#">
+          @else
+            <i class="fas fa-user-circle fa-5x"></i>
+          @endif
+        </a> 
+        <a href="{{ route('users.show',['user' => $post->user->id])}}">
+          <strong>{{ $post->user->name}}</strong>
+        </a>
+      </div>
+      <a href="{{route('users.show',['user' => $post->user->id])}}">
+        <img  width="450" class="card-img-top" src="{{ asset('storage/post_images/' . $post->photo)}}" >
+      </a>        
+        
+    </div>
+    @endforeach
+  </div>
 @endsection
