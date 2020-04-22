911b2d270c8e2100e037eea2be4a94e0d34ea6f0
benbenben1208 <ari.ka1208@gmail.com>
2020-04-22 21:07:26 +0900

    Postモデル作成　Userモデルとの一対多のリレーション

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'caption',
+        'profile_photo',
+    ];
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
