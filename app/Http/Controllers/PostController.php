f7ac31a2c107b386306887a19498f9319e43aa79
benbenben1208 <ari.ka1208@gmail.com>
2020-04-23 11:12:27 +0900

    編集ページ　削除ページ追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,5 +31,28 @@ class PostController extends Controller
         return redirect()->route('posts.index');
 
     }
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $post->caption = $request->caption;
+        $posts_store =$request->phpto->storeAs('public/post_images' ,  time() . '.jpg');
+
+        $post->photo = basename($posts_store);
+
+        $post->save();
+
+        return redirect()->route('posts.index');
+
+    }
+    public function destroy(Post $post)
+    {
+        
+        $post->delete();
+
+        return redirect()->route('posts.index');
+    }
     
 }
