65ed2024c3d8c6e4ac578c13c34af481198d6149
benbenben1208 <ari.ka1208@gmail.com>
2020-04-24 13:10:13 +0900

    PostモデルにUserモデルへの多対多のリレーション、いいね済みかどうかを判断するisLikedByメソッド、アクセサを使用しいいね数を返すgetCountLikesAttribute追加

--- app/Post.php
+++ app/Post.php
@@ -10,8 +10,23 @@ class Post extends Model
         'caption',
         'profile_photo',
     ];
+    public function likes()
+    {
+        return $this->belongsToMany('App\User', 'likes');
+    }
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+    public function isLikedBy(?User $user)
+    {
+        
+        return $user
+               ? (bool)$this->likes->where('id' , $user->id)->count()
+               : false;
+    }
+    public function getCountLikesAttribute()
+    {
+        return $this->likes->count();
     }
 }
