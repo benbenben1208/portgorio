360929975745787d4d0d6ec67605bb45ef171ca5
benbenben1208 <ari.ka1208@gmail.com>
2020-05-03 19:38:34 +0900

    各ルーティングにコメントを入れ、わかりやすくした

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Google認証ログイン　登録関連
 Route::prefix('login')->name('login.')->group(function () {
   Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
   Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
@@ -19,25 +20,30 @@ Route::prefix('register')->name('register.')->group(function () {
   Route::post('/{provider}', 'Auth\RegisterController@registerProviderUser');
 });
 
-Route::get('/', 'PostController@index')->name('posts.index');
+
 
 Auth::routes();
 
+//ユーザーページ関連
 Route::prefix('/users')->name('users.')->group(function (){
   Route::get('/{user}', 'UserController@show')->name('show');
-  Route::get('/{user}/edit', 'UserController@edit')->name('edit');
-  Route::patch('/{user}/update', 'UserController@update')->name('update');
+  Route::get('/{user}/edit', 'UserController@edit')->name('edit')->middleware('auth');
+  Route::patch('/{user}/update', 'UserController@update')->name('update')->middleware('auth');
 });
-
-Route::resource('posts','PostController')->except('index');
-
+// 投稿一覧関連
+Route::get('/', 'PostController@index')->name('posts.index');
+Route::resource('posts','PostController')->except('index', 'show')->middleware('auth');
+Route::resource('posts', 'PostController@show')->only('show');
+// いいね機能関連
 Route::put('posts/{post}/like' , 'PostController@like')->name('posts.like')->middleware('auth');
 Route::delete('posts/{post}/like' , 'PostController@unlike')->name('posts.unlike')->middleware('auth');
-
+// フォロー機能関連
 Route::put('users/{name}/follow', 'UserController@follow')->name('users.follow')->middleware('auth');
 Route::delete('users/{name}/follow', 'UserController@unfollow')->name('users.unfollow')->middleware('auth');
 
 //ここからコメント機能のルーティング
-
-Route::post('comments/','CommentController@store')->name('comments.store');
-Route::post('comments/{id}', 'CommentController@destroy')->name('comments.destroy');
+Route::prefix('/comments')->name('comments.')->middleware('auth')->group(function (){
+  Route::post('/', 'CommentController@store')->name('store');
+// Route::post('comments/','CommentController@store')->name('comments.store');
+  Route::delete('/{comment}', 'CommentController@destroy')->name('destroy');
+});
