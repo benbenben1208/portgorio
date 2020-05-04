f68e8aaa23bda9e922552e70928399842fbf40d2
benbenben1208 <ari.ka1208@gmail.com>
2020-05-04 23:14:34 +0900

    no message

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -1,11 +1,16 @@
 <div class="form-group d-flex flex-row align-items-center">
   <div>
-    <img class="rounded-circle" width="100" height="100" src="{{ asset('storage/user_images/' . Auth::user()->id . '.jpg')}}" >
+    
+    @if(Auth::user()->profile_photo)
+      <img class="rounded-circle" width="100" height="100" src="{{ asset('storage/user_images/' . Auth::user()->id . '.jpg')}}" >
+    @else
+      <i class="fas fa-user-circle fa-5x"></i>
+    @endif
+
   </div>  
   <div class="col">
-  <input type="text" placeholder="キャプションを書いてね！" name="caption" class="form-control" value="{{ $post->caption ?? old('caption')}}">
+    <input type="text" placeholder="キャプションを書いてね！" name="caption" class="form-control" value="{{ $post->caption ?? old('caption')}}">
   </div>
 </div>
-<div>
-  <input type="file" name="photo" accept="image/jpeg,image/gif,image/png" >  
-</div>
+
+  <input class="mt-3 mb-3" type="file" name="photo" accept="image/jpeg,image/gif,image/png" >  
