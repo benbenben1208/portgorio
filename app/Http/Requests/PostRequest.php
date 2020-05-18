fd3d7f459978f6b0b6a9dd942a0bdb6746b4a3fa
benbenben1208 <ari.ka1208@gmail.com>
2020-05-18 22:10:47 +0900

    PostrequestにpassedValidationメソッドを追加。
    タグはjson形式で送信されてくるため、json_decode関数で連想配列に、collectでコレクションにして、sliceでタグを5個まで追加可能として、mapで各textのみを取り出せるようにした。

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -37,4 +37,12 @@ class PostRequest extends FormRequest
             'tags' => 'タグ'
         ];
     }
+    public function passedValidation()
+    {
+        $this->tags = collect(json_decode($this->tags))
+            ->slice(0, 5)
+            ->map(function ($requestTag) {
+                return $requestTag->text;
+            })
+    }
 }
