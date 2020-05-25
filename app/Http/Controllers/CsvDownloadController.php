0661c22e863ecce7076f3faaf459c54d43b4ef7f
benbenben1208 <ari.ka1208@gmail.com>
2020-05-25 12:33:30 +0900

    凍結済みユーザーのCSVダウンロード

--- app/Http/Controllers/CsvDownloadController.php
+++ app/Http/Controllers/CsvDownloadController.php
@@ -9,10 +9,36 @@ class CsvDownloadController extends Controller
 {
    
 
-    public function download()
+    public function download_users()
     {
         $users = User::all();
         $filename = "user_profiles_".date('Ymd').".csv";
+        
+        $stream = fopen('php://temp','w');
+
+        foreach($users as $user) {
+
+            fputcsv($stream, $user->toArray());
+        }
+       
+        
+        rewind($stream);
+
+        $csv = mb_convert_encoding(str_replace(PHP_EOL, "\r\n", stream_get_contents($stream)), 'SJIS', 'UTF-8');
+        $headers = [
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachment; filename"' . $filename . '"'
+        ];
+        
+        return \Response::make($csv, 200, $headers);
+
+
+    }
+    public function download_flozened()
+    {
+        $users = User::onlyTrashed()->orderBy('deleted_at', 'desc')->get();
+        
+        $filename = "flozened_user_profiles_".date('Ymd').".csv";
         
         $stream = fopen('php://temp','w');
 
